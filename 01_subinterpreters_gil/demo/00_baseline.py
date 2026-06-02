88a6ea893c4155f7a9abc9ca733d15bd3c7e593b
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-06-02 11:01:04 +0300

    Add demos for talk 01

---
+++ 01_subinterpreters_gil/demo/00_baseline.py
@@ -0,0 +1,35 @@
+import math
+import sys
+import time
+
+
+NUMBERS = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099
+] * 10
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    print(sys.version)
+    start = time.time()
+    for number in NUMBERS:
+        is_prime(number)
+    print(f"Single CPU took {time.time() - start:.2f} seconds")
---
+++ 01_subinterpreters_gil/demo/03_multiinterpreter.py
@@ -0,0 +1,40 @@
+import math
+import sys
+import time
+
+from concurrent.futures import InterpreterPoolExecutor
+
+NUMBERS = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099
+] * 10
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    print(sys.version)
+    start = time.time()
+
+    workers = int(sys.argv[1]) if len(sys.argv) > 1 else 4
+
+    with InterpreterPoolExecutor(max_workers=workers) as executor:
+        executor.map(is_prime, NUMBERS)
+
+    print(f"{workers} workers took {time.time() - start:.2f} seconds")
